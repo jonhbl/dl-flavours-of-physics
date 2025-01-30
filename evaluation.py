902d844d0c020cb2c70db395399a2c977e524a87
Jonh Brian Lemos <jonh.lemos@grupoboticario.com.br>
2025-01-30 12:15:43 -0300

    result yeah

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from sklearn.metrics import roc_curve, auc
 
 
@@ -15,7 +15,7 @@ def __rolling_window(data, window_size):
     """
     shape = data.shape[:-1] + (data.shape[-1] - window_size + 1, window_size)
     strides = data.strides + (data.strides[-1],)
-    return numpy.lib.stride_tricks.as_strided(data, shape=shape, strides=strides)
+    return np.lib.stride_tricks.as_strided(data, shape=shape, strides=strides)
 
 
 def __cvm(subindices, total_events):
@@ -28,14 +28,12 @@ def __cvm(subindices, total_events):
     :param total_events: count of events in the second distribution
     :return: cvm metric
     """
-    target_distribution = (
-        numpy.arange(1, total_events + 1, dtype="float") / total_events
-    )
-    subarray_distribution = numpy.cumsum(
-        numpy.bincount(subindices, minlength=total_events), dtype="float"
+    target_distribution = np.arange(1, total_events + 1, dtype="float") / total_events
+    subarray_distribution = np.cumsum(
+        np.bincount(subindices, minlength=total_events), dtype="float"
     )
     subarray_distribution /= 1.0 * subarray_distribution[-1]
-    return numpy.mean((target_distribution - subarray_distribution) ** 2)
+    return np.mean((target_distribution - subarray_distribution) ** 2)
 
 
 def compute_cvm(predictions, masses, n_neighbours=200, step=50):
@@ -49,23 +47,23 @@ def compute_cvm(predictions, masses, n_neighbours=200, step=50):
     :param step: step through sorted mass-array to define next center of bin
     :return: average cvm value
     """
-    predictions = numpy.array(predictions)
-    masses = numpy.array(masses)
+    predictions = np.array(predictions)
+    masses = np.array(masses)
     assert len(predictions) == len(masses)
 
     # First, reorder by masses
-    predictions = predictions[numpy.argsort(masses)]
+    predictions = predictions[np.argsort(masses)]
 
     # Second, replace probabilities with order of probability among other events
-    predictions = numpy.argsort(
-        numpy.argsort(predictions, kind="mergesort"), kind="mergesort"
+    predictions = np.argsort(
+        np.argsort(predictions, kind="mergesort"), kind="mergesort"
     )
 
     # Now, each window forms a group, and we can compute contribution of each group to CvM
     cvms = []
     for window in __rolling_window(predictions, window_size=n_neighbours)[::step]:
         cvms.append(__cvm(subindices=window, total_events=len(predictions)))
-    return numpy.mean(cvms)
+    return np.mean(cvms)
 
 
 def __roc_curve_splitted(data_zero, data_one, sample_weights_zero, sample_weights_one):
@@ -79,8 +77,8 @@ def __roc_curve_splitted(data_zero, data_one, sample_weights_zero, sample_weight
     :return: roc curve
     """
     labels = [0] * len(data_zero) + [1] * len(data_one)
-    weights = numpy.concatenate([sample_weights_zero, sample_weights_one])
-    data_all = numpy.concatenate([data_zero, data_one])
+    weights = np.concatenate([sample_weights_zero, sample_weights_one])
+    data_all = np.concatenate([data_zero, data_one])
     fpr, tpr, _ = roc_curve(labels, data_all, sample_weight=weights)
     return fpr, tpr
 
@@ -102,26 +100,24 @@ def compute_ks(data_prediction, mc_prediction, weights_data, weights_mc):
         weights_mc
     ), "Data length and weight one must be the same"
 
-    data_prediction, mc_prediction = numpy.array(data_prediction), numpy.array(
-        mc_prediction
-    )
-    weights_data, weights_mc = numpy.array(weights_data), numpy.array(weights_mc)
+    data_prediction, mc_prediction = np.array(data_prediction), np.array(mc_prediction)
+    weights_data, weights_mc = np.array(weights_data), np.array(weights_mc)
 
-    assert numpy.all(data_prediction >= 0.0) and numpy.all(
+    assert np.all(data_prediction >= 0.0) and np.all(
         data_prediction <= 1.0
     ), "Data predictions are out of range [0, 1]"
-    assert numpy.all(mc_prediction >= 0.0) and numpy.all(
+    assert np.all(mc_prediction >= 0.0) and np.all(
         mc_prediction <= 1.0
     ), "MC predictions are out of range [0, 1]"
 
-    weights_data /= numpy.sum(weights_data)
-    weights_mc /= numpy.sum(weights_mc)
+    weights_data /= np.sum(weights_data)
+    weights_mc /= np.sum(weights_mc)
 
     fpr, tpr = __roc_curve_splitted(
         data_prediction, mc_prediction, weights_data, weights_mc
     )
 
-    Dnm = numpy.max(numpy.abs(fpr - tpr))
+    Dnm = np.max(np.abs(fpr - tpr))
     return Dnm
 
 
@@ -133,14 +129,13 @@ def roc_auc_truncated(
 ):
     """
     Compute weighted area under ROC curve.
-
     :param labels: array-like, true labels
     :param predictions: array-like, predictions
     :param tpr_thresholds: array-like, true positive rate thresholds delimiting the ROC segments
     :param roc_weights: array-like, weights for true positive rate segments
     :return: weighted AUC
     """
-    assert numpy.all(predictions >= 0.0) and numpy.all(
+    assert np.all(predictions >= 0.0) and np.all(
         predictions <= 1.0
     ), "Data predictions are out of range [0, 1]"
     assert len(tpr_thresholds) + 1 == len(
@@ -150,16 +145,12 @@ def roc_auc_truncated(
     area = 0.0
     tpr_thresholds = [0.0] + list(tpr_thresholds) + [1.0]
     for index in range(1, len(tpr_thresholds)):
-        tpr_cut = numpy.minimum(tpr, tpr_thresholds[index])
-        tpr_previous = numpy.minimum(tpr, tpr_thresholds[index - 1])
-        area += roc_weights[index - 1] * (
-            auc(fpr, tpr_cut, reorder=True) - auc(fpr, tpr_previous, reorder=True)
-        )
-    tpr_thresholds = numpy.array(tpr_thresholds)
+        tpr_cut = np.minimum(tpr, tpr_thresholds[index])
+        tpr_previous = np.minimum(tpr, tpr_thresholds[index - 1])
+        area += roc_weights[index - 1] * (auc(fpr, tpr_cut) - auc(fpr, tpr_previous))
+    tpr_thresholds = np.array(tpr_thresholds)
     # roc auc normalization to be 1 for an ideal classifier
-    area /= numpy.sum(
-        (tpr_thresholds[1:] - tpr_thresholds[:-1]) * numpy.array(roc_weights)
-    )
+    area /= np.sum((tpr_thresholds[1:] - tpr_thresholds[:-1]) * np.array(roc_weights))
     return area
 
 
